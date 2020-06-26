eb053215d7a6777888f9ff422626053503f159aa
Peter Law <PeterJCLaw@gmail.com>
2020-06-26 16:26:42 +0100

    Pre-create a test client for each test

--- tests/tests_app.py
+++ tests/tests_app.py
@@ -5,14 +5,22 @@ from starlette.testclient import TestClient
 
 
 class AppTests(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        test_client = TestClient(app)
+        self.session = test_client.__enter__()
+
+    def tearDown(self) -> None:
+        self.session.__exit__(None, None, None)
+        super().tearDown()
+
     def test_app(self) -> None:
-        client = TestClient(app)
-        response = client.get('/')
+        response = self.session.get('/')
         self.assertEqual(200, response.status_code)
 
     def test_upload_file(self) -> None:
-        client = TestClient(app)
-        response = client.post(
+        response = self.session.post(
             '/upload',
             files={'archive': ('whatever.zip', b'should-be-a-zip', 'application/zip')},
         )
