d0c031d72011e90c571919022e8a18b9d433c26b
Peter Law <PeterJCLaw@gmail.com>
2021-01-01 01:49:57 +0000

    Remove unused noqa comments

--- code_submitter/tables.py
+++ code_submitter/tables.py
@@ -5,7 +5,7 @@ metadata = sqlalchemy.MetaData()
 Archive = sqlalchemy.Table(
     'archive',
     metadata,
-    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),  # noqa:A003
+    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column('content', sqlalchemy.LargeBinary, nullable=False),
 
     sqlalchemy.Column('username', sqlalchemy.String, nullable=False),
@@ -22,7 +22,7 @@ Archive = sqlalchemy.Table(
 ChoiceHistory = sqlalchemy.Table(
     'choice_history',
     metadata,
-    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),  # noqa:A003
+    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column('archive_id', sqlalchemy.ForeignKey('archive.id'), nullable=False),
 
     sqlalchemy.Column('username', sqlalchemy.String, nullable=False),
