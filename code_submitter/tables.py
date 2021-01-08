3b0a3d3f536da2bdf0e70e6384043e1464ea3b2a
Peter Law <PeterJCLaw@gmail.com>
2021-01-08 23:16:10 +0000

    Initial introduction of models for sessions
    This introduces the concept of a Session, which may or not map
    exactly to an actual session of matches, that a given collection
    of archives is used for.
    
    With thanks to James for design discussion.
    
    Co-Authored-By: James Seden Smith <sedders123@gmail.com>

--- code_submitter/tables.py
+++ code_submitter/tables.py
@@ -38,3 +38,38 @@ ChoiceHistory = sqlalchemy.Table(
         server_default=sqlalchemy.func.now(),
     ),
 )
+
+
+# At the point of downloading the archives in order to run matches, you create a
+# Session. The act of doing that will also create the required ChoiceForSession
+# rows to record which items will be contained in the download.
+Session = sqlalchemy.Table(
+    'session',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column('description', sqlalchemy.String, nullable=False),
+
+    sqlalchemy.Column('username', sqlalchemy.String, nullable=False),
+
+    sqlalchemy.Column(
+        'created',
+        sqlalchemy.DateTime(timezone=True),
+        nullable=False,
+        server_default=sqlalchemy.func.now(),
+    ),
+)
+
+ChoiceForSession = sqlalchemy.Table(
+    'choice_for_session',
+    metadata,
+    sqlalchemy.Column(
+        'choice_id',
+        sqlalchemy.ForeignKey('choice_history.id'),
+        primary_key=True,
+    ),
+    sqlalchemy.Column(
+        'session_id',
+        sqlalchemy.ForeignKey('session.id'),
+        primary_key=True,
+    ),
+)
