0a066b1f469f74fc5b0f07ba50dc54b38f225172
Peter Law <PeterJCLaw@gmail.com>
2022-05-24 19:37:32 +0100

    Move to current Python versions only

--- setup.py
+++ setup.py
@@ -37,11 +37,12 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 
     install_requires=install_requires,
 )
